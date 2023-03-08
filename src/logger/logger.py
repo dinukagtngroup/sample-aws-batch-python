2f2aa1d343908c2507c68519c2080b071e4f1225
DinukaNimantha <n.dinuka@gtngroup.com>
2023-03-08 14:36:31 +0530

    Added region in boto3

--- src/logger/logger.py
+++ src/logger/logger.py
@@ -4,7 +4,7 @@ import boto3
 
 
 def get_batch_client():
-    return boto3.client('batch')
+    return boto3.client('batch', region='ap-south-1')
 
 
 def tag_job_multiple(job_arn: str, tags: dict[str]):
