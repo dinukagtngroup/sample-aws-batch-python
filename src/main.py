c4264065b6f35db5b39628b368cd6b77560fb379
DinukaNimantha <n.dinuka@gtngroup.com>
2023-03-10 10:33:15 +0530

    Modified main.py

--- src/main.py
+++ src/main.py
@@ -1,3 +1,4 @@
+import os
 import sys
 
 from logger import *
@@ -8,6 +9,12 @@ count = 0
 for i in range(100):
     count += i
 print("Count is - " + str(count))
+
+
+print(os.listdir('/'))
+
+print('OS Environment', os.environ)
+print('AWS Default Region', os.environ['AWS_DEFAULT_REGION'])
 
 try:
     div = 10/0
